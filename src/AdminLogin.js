12a19be34b143c6a0c7fe64ab0657b45b0a20e50
abinaya <abinayacse07@gmail.com>
2026-03-10 21:02:25 +0530

    initial commit

---
+++ src/AdminLogin.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./AdminLogin.css";
+
+function AdminLogin() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if(email === "admin@gmail.com" && password === "admin123"){
+      alert("Admin Login Successful");
+    }else{
+      alert("Invalid Email or Password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+
+      <h2>Sleepin Sync Admin Login</h2>
+
+      <form onSubmit={handleLogin}>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+
+      </form>
+
+    </div>
+  );
+}
+
+export default AdminLogin;
